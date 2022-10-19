c7d9246bfdab84bb2876bcd2bae92553f90c49c7
Brandddd <b.palacio1@utp.edu.co>
2022-10-19 17:04:52 -0500

    campos

--- ExecutorService/src/com/example/client/NetworkClientCallable.java
+++ ExecutorService/src/com/example/client/NetworkClientCallable.java
@@ -5,12 +5,21 @@ import java.net.Socket;
 
 public class NetworkClientCallable implements Callable<RequestResponse>{
 
-    RequestResponse request = new RequestResponse(null, 0);
-
-    public NetworkClientCallable() {
+    public RequestResponse reference;   // * Tipo y nombre del campo.
+    
+    public NetworkClientCallable(RequestResponse reference) {    // * En el constructor se le envía el tipo de referencia y la referencia 
+        this.reference = reference;
     }
 
     public RequestResponse call() throws Exception {
+
+        Socket socket = new Socket(this.reference.host, this.reference.port);  // * Se crea el socket con la ref de RequestResponse
+        System.out.println("Hola soy un cliente en el socket: ");
+        System.out.println(socket);
+
+        socket.close();
+
         return null;
+
     }
 }
