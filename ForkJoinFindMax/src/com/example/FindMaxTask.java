84342e9be7b9a9245e53a0e9c6b5cdf3d83ff246
Brandddd <b.palacio1@utp.edu.co>
2022-10-21 19:51:55 -0500

    adding-some-comments

--- ForkJoinFindMax/src/com/example/FindMaxTask.java
+++ ForkJoinFindMax/src/com/example/FindMaxTask.java
@@ -15,7 +15,18 @@ public class FindMaxTask extends RecursiveTask<Integer> {
         this.end = end;
     }
 
+    /*
+     * Método compute, no recibe ningún parámetro
+     * Retorna un entero
+     * 
+     */
+
     protected Integer compute() {
+
+        System.out.println("start: " + start);
+        System.out.println("end: " + end);
+        System.out.println("threshold: " + threshold);
+
         if (end - start < threshold) {
             int max = Integer.MIN_VALUE;
             for (int i = start; i <= end; i++) {
@@ -26,10 +37,13 @@ public class FindMaxTask extends RecursiveTask<Integer> {
             }
             return max;
         } else {
-            int midway = (end - start) / 2 + start;
+            int midway = (end - start) / 2 + start; // * Tecnica divide y vencerás para dividirlo en dos procesos Fork.
+            // * Llamando nuevamente la clase FindMaxTask pero esta vez con la primera mitad de la matriz.
             FindMaxTask a1 = new FindMaxTask(myArray, start, midway, threshold);
-            a1.fork();
-            FindMaxTask a2 = new FindMaxTask(myArray, midway + 1, end, threshold);           
+            a1.fork(); // * Mandando la matriz al proceso 
+            // * Llamando nuevamente la clase FindMaxTask pero esta vez con la segunda mitad de la matriz.
+            FindMaxTask a2 = new FindMaxTask(myArray, midway + 1, end, threshold);
+            // * Retorna el número máximo que hay entre las dos mitades
             return Math.max(a2.compute(), a1.join());
         }
     }
--- ForkJoinFindMax/src/com/example/Main.java
+++ ForkJoinFindMax/src/com/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int[] data = new int[1024 * 1024 * 128]; //512MB
 
         for (int i = 0; i < data.length; i++) {
-            data[i] = ThreadLocalRandom.current().nextInt();
+            data[i] = ThreadLocalRandom.current().nextInt();  // * Generador de numeros aleatorios dentro de la matriz data
         }
 
 //        int max = Integer.MIN_VALUE;
@@ -22,10 +22,18 @@ public class Main {
 //        }
 //        System.out.println("Max value found:" + max);
         
-        ForkJoinPool pool = new ForkJoinPool();
-        FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);
+        ForkJoinPool pool = new ForkJoinPool();     // * Invoca una tarea de tipo forkJoinTask.
+        FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);  // *Invoca clase FindMaxTask
         Integer result = pool.invoke(task);
         System.out.println("Max value found:" + result);
         
     }
 }
+
+
+/*
+ * Para poder invocar múltiples subtareas en paralelo de forma recursiva, invocará a una tarea
+ * de tipo RecursiveAction, que extiende de ForkJoinTask.
+ * La clase RecursiveACtion contiene el método compute(). la cual será la encargada de de ejecutar
+ * nuestra tarea paralelizable.
+ */
